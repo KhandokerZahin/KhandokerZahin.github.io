5e25b3c8ef17ce8544d1833961a27c92354e9966
KhandokerZahin <zahintasic@gmail.com>
2025-04-14 13:35:23 -0400

    Push website without bootstrap

---
+++ Lesson 3 Wild Life/lab2/rescues-bootstrap/scripts.js
@@ -0,0 +1,9 @@
+/*
+    Student Name: Khandoker Zahin
+    File Name: script.js
+    Date: 04/12/2025
+*/
+//jQuery for hero image to consume the header window space
+$(document) .ready (function(){
+    $('.hero') .height ($(window) .height());
+});
