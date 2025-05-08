a41b48cd729140a950e145c83e7800fce232f25d
KhandokerZahin <zahintasic@gmail.com>
2025-05-07 23:01:25 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// Global variables
+var answer = document.querySelector("#answer p");
+var heading = document.querySelector("#answer h2");
+
+// Function to display the fourth answer
+function ans4 () {
+    heading.style.display = "block";
+    answer.textContent = "My dream is to be an Software Engineer!";
+}
+
+// Function to display the first answer
+function ans1 () {
+    heading.style.display = "block";
+    answer.textContent = "Ever since I was 3 years old!!!.";
+}
+
+// Function to display the second answer
+function ans2 () {
+    heading.style.display = "block";
+    answer.textContent = "You cannot tell if an animal has rabies simply by seeing it. A test must be performed to determine if an animal has rabies. Do not approach wildlife that you suspect might be rabid. Contact us to have the animal removed.";
+}
+
+// Function to display the third answer
+function ans3 () {
+    heading.style.display = "block";
+    answer.textContent = "By taking a Web Development class and made me passionate about it.";
+}
